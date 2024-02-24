1c1ec09d216502623e78db1971fa991ee86f39d0
kunalpere <pere.kunal9@gmail.com>
2024-02-24 18:33:06 +0530

    python code

---
+++ kunal.py
@@ -0,0 +1,10 @@
+
+# your profile name code inserted..!!
+
+name = str(input(" Enter your name here : "))
+surname = str( input (" Enter your surname here: "))
+
+your_profile = (name+surname) 
+print(your_profile)
+
+
